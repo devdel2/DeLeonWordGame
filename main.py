45610b63807887c5e7c6eed5472be627f9ddb87a
devdel2 <ddeleon2@uccs.edu>
2023-07-16 21:11:56 -0600

    added python imports for basic webscraping

--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-print("Hello World!")
+"""
+    Name: Devin DeLeon
+    Date: 7/16/2023
+    Description: For now, this file will act as the base of my python project. This project
+    will be to parse a website to gather a list of words and create a word game
+    out of this list of words.
+"""
+
+import requests, bs4
+
